a82b0d6259b5e36b1160d810f4f6316bb9d5f604
Alejandro Saiz <Alejandro Saiz@DESKTOP-BE2J2HT>
2022-10-20 14:06:20 +0200

    entitiesCreation

--- TrabajoISO2/src/negocio/entities/ProfesorExterno.java
+++ TrabajoISO2/src/negocio/entities/ProfesorExterno.java
@@ -2,6 +2,22 @@ package negocio.entities;
 
 public class ProfesorExterno extends Profesor {
 
+
 	private String titulacion;
+
+	public ProfesorExterno(String dni, String nombre, String apellidos, boolean doctor, String titulacion) {
+		super(dni, nombre, apellidos, doctor);
+		this.titulacion = titulacion;
+	}
+
+	public String getTitulacion() {
+		return titulacion;
+	}
+
+	public void setTitulacion(String titulacion) {
+		this.titulacion = titulacion;
+	}
+	
+	
 
 }

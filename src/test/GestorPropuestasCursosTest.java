e1e13b9da2b0f5b41510bd8998eb9470b206fdbd
AlejandroRJBatuecas <alejandroramiro.jimenez@alu.uclm.es>
2022-12-26 15:05:43 +0100

    Creación del test de GestorPropuestasCursos

---
+++ src/test/GestorPropuestasCursosTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import org.junit.*;
+
+public class GestorPropuestasCursosTest {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+	
+	@Test
+
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+
+}
